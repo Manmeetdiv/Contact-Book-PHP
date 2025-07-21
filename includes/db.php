492cd556d97e7d1e29efcd78d207d6de5a9a8206
Manmeet Singh <33478633+Manmeetdiv@users.noreply.github.com>
2025-07-21 23:51:19 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$db = "contact_book";
+$user = "root";
+$pass = "";
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("DB connection failed: " . $e->getMessage());
+}
+?>
